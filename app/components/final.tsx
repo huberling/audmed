338534afbec224562aa0ff5beb84fd1b39246524
AlexjrFS <afortunatosacramento@gmail.com>
2024-01-17 09:47:39 -0300

    17/01

--- app/components/final.tsx
+++ app/components/final.tsx
@@ -17,7 +17,6 @@ export default function Final(){
         <div className="roww">
         <div className="col-md-3 mt-8 mb-16 ml-14">
             <Image className='w-44 h-20 mt-16 inline-block ml-16' src={LogoClinica} alt="Logo Clinica"/>
-    
         </div>
         <div className="col-md-3 mt-16 mb-16 font-family: 'Lato', sans-serif">
             <h4> 
@@ -27,7 +26,6 @@ export default function Final(){
             <p className='md:mr-0 lg:mr-0 mr-6 ml-8 md:ml-5 lg:ml-5'><FontAwesomeIcon icon={faPhone} className="mr-1 ml-1"></FontAwesomeIcon>Telefone:<Link href={'tel:+55193894-5066'}>+55 (19) 3894-5066</Link></p>
             <p className='md:mr-0 lg:mr-0 mr-6 ml-8 md:ml-5 lg:ml-5'><FontAwesomeIcon icon={faWhatsapp} className="mr-1 ml-1" ></FontAwesomeIcon>Whatsapp:<Link href="https://wa.me/5519992492716" target='_blank' rel="noopener noreferrer">+55 (19) 99249-2716</Link></p>
             <p className='md:mr-0 lg:mr-0 mr-6 ml-8 md:ml-5 lg:ml-5'><FontAwesomeIcon icon={faLocationDot} className="mr-1 ml-1"></FontAwesomeIcon>Endereço:<Link href="https://www.google.com/maps?q=Rua+Ademar+de+Barros,+491,+Centro,+13330-130,+Indaiatuba,+SP" target="_blank" rel="noopener noreferrer"> Rua Ademar de Barros, 491 - <br/> Centro - 13330-130 - Indaiatuba/SP </Link></p>
-          
         </div>
         <div className="navegacao col-md-2 mt-16 mb-16">
             <h4><b className='ml-8 md:ml-5 lg:ml-5'>Navegação</b></h4>
@@ -65,7 +63,6 @@ export default function Final(){
         </div>
         </div>
     </section>
- 
         </>
     )
 }
