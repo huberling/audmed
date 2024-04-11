27f830230c3ea21637ce2179ecb8208573f43866
AlexjrFS <afortunatosacramento@gmail.com>
2024-04-11 09:36:00 -0300

    .

--- app/trabalhe-conosco/page.tsx
+++ app/trabalhe-conosco/page.tsx
@@ -31,7 +31,7 @@ export default function TrabalheConosco(){
     </section>  
     <section className="container md:flex">
         <div className="descritivos col-md-6">
-            <h3 className="text-lg font-semibold mb-3 leading-6 ml-4 md:ml-0 lg:ml-0 md:mt-2">Nossa Missão</h3>
+            <h3 className="text-lg font-semibold mb-3 leading-6 ml-4 md:ml-0 lg:ml-0 md:mt-2">Nosso Objetivo</h3>
             <p className="text2 ml-4 md:ml-0 lg:ml-0 mr-4 md:mr-0 lg:mr-0 ">Na Clínica Audmed, estamos constantemente em busca de novos talentos para se juntarem à nossa equipe dedicada a promover a saúde e a segurança no ambiente de trabalho, com o objetivo de assegurar a integridade física dos colaboradores, com o máximo de excelência.</p>
         </div>
         <div className="descritivos col-md-4 ">
